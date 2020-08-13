9d17378bc8eea9b6692a34d6810d2aa8d0b87a71
Julie Schaub <julie.schaub@hotmail.com>
2020-08-12 19:41:55 -0500

    updated files

--- Develop/app.js
+++ Develop/app.js
@@ -1,9 +1,81 @@
+/* Pseudocode
+1. Software  engineering team generator
+2. Develop prompts for team manager
+3. Create a base class called Employee
+    a.Prompts for Employee
+        i. User name
+        ii. id
+        iii. email
+        iv. getName()
+        v. getId()
+        vi. getEmail()
+        vii. getRole() -- returns 'Employee'
+    b. Prompts for Manager will also include:
+        i. officeNumber
+        ii. getRole() --Overridden to return 'Manager'
+    c. Prompts for Engineer will also include:
+        i. github --GitHub username
+        ii. getGitHub()
+        iii. getRole -- Overridden to return Engineer
+    d. Prompts for Intern will also include:
+        i. school
+        ii. getSchool()
+        iii. getRole() -- Overridden to return Intern
+4. Text
+5. Create an HTML file that displays a team roster with basic info.
+6. In develop folder run npm install
+    a. Install inquirer
+    b. Install jest
+7. Recommended workflow:
+    a. Run tests - npm run test
+    b. Create or update classes to pass a single text case
+    c. Repeat
+8. Recommended directory structure:
+    a. lib/     //classes and helper code
+    b. output/  //rendered output = team.html
+    c. templates/ //HTML template(s)
+        a. engineer
+        b. intern
+        c. main -- Employee?
+        d. manager
+    d. test/    //jest tests
+        i. Employee.test.js
+        ii. Engineer.test.js
+        iii. Intern.test.js
+        iv. Manager.test.js
+    e. app.js   //Runs the application
+9. Create HTML templates, create placeholder character the identifies where dynamic markup begins and ends:
+    a. main.html
+    b. engineer.html
+    c. intern.html
+    d. manager.html
+10. Prompt user to build a team consisting of:
+    a. Manager
+    b. Engineers - any number
+    c. Interns - any number
+11. Generate team.html page in output
+12. Each team member needs to include:
+    a. Name
+    b. Role
+    c. Id
+    d. Role-specific property (school, link to GitHub profile or office number)
+13. Use Validatio to ensure that info provided is in proper format - strings, numbers, etc
+14. Add app to portfolio*/
+
+
+
+
+
+
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const inquirer = require('inquirer');
+const jest = require('jest');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
