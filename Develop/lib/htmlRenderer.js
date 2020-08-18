5fcecd43e360e6eaf87f966c3e95ad7c76e63cf1
Julie Schaub <julie.schaub@hotmail.com>
2020-08-18 10:58:36 -0500

    updated files

--- Develop/lib/htmlRenderer.js
+++ Develop/lib/htmlRenderer.js
@@ -6,16 +6,13 @@ const templatesDir = path.resolve(__dirname, "../templates");
 const render = employees => {
   const html = [];
 
-  html.push(employees
-    .filter(employee => employee.getRole() === "Manager")
+  html.push(employees.filter(employee => employee.getRole() === "Manager")
     .map(manager => renderManager(manager))
   );
-  html.push(employees
-    .filter(employee => employee.getRole() === "Engineer")
+  html.push(employees.filter(employee => employee.getRole() === "Engineer")
     .map(engineer => renderEngineer(engineer))
   );
-  html.push(employees
-    .filter(employee => employee.getRole() === "Intern")
+  html.push(employees.filter(employee => employee.getRole() === "Intern")
     .map(intern => renderIntern(intern))
   );
 
